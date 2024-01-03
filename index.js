4bcdb981a911997f319907d0e152fc56740edae0
Carl Egonu <carlegonu@Carls-MacBook-Air.local>
2024-01-03 16:04:12 +0000

    get csv-parse working

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 import express from 'express';
 import axios from 'axios';
 import bodyParser from 'body-parser';
+import { parse } from 'csv-parse';
+import fs from 'fs';
 
 const app = express();
 const port = 3000;
+const data = [];
+const parser = parse({
+    delimiter: ','
+});
+
+fs.createReadStream('./worldcities.csv')
+.pipe(parse({delimiter: ',', from_line: 2}))
+.on('data', (row) =>{
+    data.push(row);
+})
+.on('end', () => {
+    console.log('finished');
+    console.log(data);
+})
+.on('error', (error) =>{
+    console.log(error.message);
+});
+
+app.use(express.static('public'));
 
 app.get('/',(req,res) =>{
-    res.render('index.ejs')
+    
 });
 
 app.listen(port, () =>{
