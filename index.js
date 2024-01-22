835addeacb5d0d6ecf74304ab07749e313f65772
Carl Egonu <egonucarl@gmail.com>
2024-01-22 10:04:05 +0000

    scrap suggestion work on information transfer

--- index.js
+++ index.js
@@ -1,33 +1,32 @@
 import express from 'express';
 import axios from 'axios';
 import bodyParser from 'body-parser';
-import { parse } from 'csv-parse';
-import fs from 'fs';
+import {readFile} from 'node:fs';
 
 const app = express();
 const port = 3000;
-const data = [];
-const parser = parse({
-    delimiter: ','
-});
-
-fs.createReadStream('./worldcities.csv')
-.pipe(parse({delimiter: ',', from_line: 2}))
-.on('data', (row) =>{
-    //Create an object for each location. Object has 3 properties cityName, latitude and longitude.
-    const city = {cityName: row[0], lat: row[1], lng: row[2]};
-    data.push(city);
-})
-.on('end', () => {
-    console.log('finished');
-})
-.on('error', (error) =>{
-    console.log(error.message);
-});
+let datesTimesAndTemp = {};
 
 app.use(express.static('public'));
 
+function getDayTimeAndTemp(dateList, tempList){
+    let list = [];
+    for (let index = 0; index < dateList.length; index += 25) {
+        list.push(dateList[index].slice(8,10));
+    }
+    datesTimesAndTemp.dates = list;
+    console.log(datesTimesAndTemp)
+}
+
 app.get('/',(req,res) =>{
+    axios({
+        method: 'get',
+        url: '/forecast?latitude=51.558&longitude=-1.7812&hourly=temperature_2m',
+        baseURL: 'https://api.open-meteo.com/v1'
+    })
+    .then(function (response){
+        getDayTimeAndTemp(response.data.hourly.time, response.data.temperature_2m)
+    })
     res.render('index.ejs');
 });
 
