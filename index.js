a01c72022a39280e8fd3652aa67600a5bfbfe470
Carl Egonu <egonucarl@gmail.com>
2024-03-10 14:34:07 +0000

    create function to get average temperatures

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import countries from './worldcities.json' assert { type: 'json'};
 const app = express();
 const port = 3000;
 let datesTimesAndTemp = {};
+let averageTempList = [];
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
@@ -26,8 +27,22 @@ function getDayTimeAndTemp(dateList, tempList){
     }
     datesTimesAndTemp.times = list;
     datesTimesAndTemp.temp = tempList;
-    console.log(datesTimesAndTemp);
-}
+};
+
+//Create average temperature for each day of the week
+function getDayAverageTemp(tempList){
+    console.log("getDayAverageTemp is running")
+    let totalTemp = 0;
+    for(let i = 0; i < 168; i += 24){ //24 hours in a day, 7 days in a week. 24 x 7 = 168 total temperatures
+        for(let y = i; y < i + 24; y += 1){ //want to start counting from the week we're starting at so y = i
+            totalTemp += tempList[y]; //add the value at templist[y] to the total temp
+        };
+        totalTemp = Math.round(totalTemp/24); //divide the total temp by the number of temperatures in a day (24) then round up or down the closest whole number
+        averageTempList.push(totalTemp); //push the whole integer to an array, should end up with 7 whole numbers
+        totalTemp = 0;
+    };
+    datesTimesAndTemp.averageTemperatures = averageTempList;
+};
 
 app.get('/',(req,res) =>{
     res.render('index.ejs',{data: datesTimesAndTemp});
@@ -44,9 +59,10 @@ app.post('/country', (req, res) =>{
     })
     .then(function (response){
         getDayTimeAndTemp(response.data.hourly.time, response.data.hourly.temperature_2m)
+        getDayAverageTemp(datesTimesAndTemp.temp)
         res.render('index.ejs',{data: datesTimesAndTemp});
     });
-})
+});
 
 app.listen(port, () =>{
     console.log(`Listening on port ${port}`)
