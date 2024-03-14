fc16012723efd0c8ef90f0d32145b2e10ccdaac5
Carl Egonu <egonucarl@gmail.com>
2024-03-14 18:29:04 +0000

    WIP get most frequesnt weather codes

--- index.js
+++ index.js
@@ -5,33 +5,31 @@ import countries from './worldcities.json' assert { type: 'json'};
 
 const app = express();
 const port = 3000;
-let datesTimesAndTemp = {};
+let data = {};
 let averageTempList = [];
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
-//gets 
-function getDayTimeAndTemp(dateList, tempList){
+//Creates times for each hour of the day.
+function getDates(dateList){
     let list = [];
     for (let index = 0; index < dateList.length; index += 25) {
         list.push(dateList[index].slice(8,10));
-    }
-    datesTimesAndTemp.dates = list;
+    };
+    data.dates = list;
     list = [];
     for (let index = 0; index < 10; index++) {
         list.push(`0${index}00`);
-    }
+    };
     for (let index = 10; index < 24; index++) {
         list.push(`${index}00`);
-    }
-    datesTimesAndTemp.times = list;
-    datesTimesAndTemp.temp = tempList;
+    };
+    data.times = list;
 };
 
 //Create average temperature for each day of the week
 function getDayAverageTemp(tempList){
-    console.log("getDayAverageTemp is running")
     let totalTemp = 0;
     for(let i = 0; i < 168; i += 24){ //24 hours in a day, 7 days in a week. 24 x 7 = 168 total temperatures
         for(let y = i; y < i + 24; y += 1){ //want to start counting from the week we're starting at so y = i
@@ -41,29 +39,40 @@ function getDayAverageTemp(tempList){
         averageTempList.push(totalTemp); //push the whole integer to an array, should end up with 7 whole numbers
         totalTemp = 0;
     };
-    datesTimesAndTemp.averageTemperatures = averageTempList; //create new object property setting it to list of average temperatures
+    data.averageTemperatures = averageTempList; //create new object property setting it to list of average temperatures
 };
+
+function getAverageWeatherCode(weathercodes){
+    console.log("getAverageWeatherCode running");
+    for(let i = 0; i < 168; i += 24){
+        let mf = 1;
+        let compare = 0;
+        for(let j = i + 1; j < i + 24; j++){
+
+        }
+    }
+}
 
 app.get('/',(req,res) =>{
     res.render('index.ejs');
 });
 
 app.post('/country', (req, res) =>{
-    console.log(req.body.myCountry);
+    console.log(req.body.myCity);
     let city = countries.filter((hit) => hit.city === req.body.myCity);
     city = city[0];
     axios({
         method: 'get',
-        url: `/forecast?latitude=${city.lat}&longitude=${city.lng}&hourly=temperature_2m`,
+        url: `/forecast?latitude=${city.lat}&longitude=${city.lng}&hourly=temperature_2m,weather_code`,
         baseURL: 'https://api.open-meteo.com/v1'
     })
     .then(function (response){
-        getDayTimeAndTemp(response.data.hourly.time, response.data.hourly.temperature_2m)
-        getDayAverageTemp(datesTimesAndTemp.temp)
-        res.render('index.ejs',{data: datesTimesAndTemp});
+        getDates(response.data.hourly.time);
+        getDayAverageTemp(data.temp);
+        res.render('index.ejs',{data: data});
     });
 });
 
 app.listen(port, () =>{
-    console.log(`Listening on port ${port}`)
+    console.log(`Listening on port ${port}`);
 });
